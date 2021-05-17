8651a7e652e8fa130b63aaf7b69735ac5392b5f6
Sol-Cabrera <80895344+Sol-Cabrera@users.noreply.github.com>
2021-05-17 13:15:45 -0300

    Update Ejercicio2TLP.java

--- Ejercicio2TLP.java
+++ Ejercicio2TLP.java
@@ -51,18 +51,18 @@ public Estudiantes (){}
 public static void main (String args[]){ 
   int a=1;
   int i;
-  Estudiantes[] estudiante= new Estudiantes[a];
+  Estudiantes[] alumno= new Estudiantes[a];
   for(i=0; i<a; i++){
- estudiante[i] = new Estudiantes();
- estudiante[i].setLegajo(Integer.valueOf(JOptionPane.showInputDialog("Ingrese legajo")));
- estudiante[i].setNombre(JOptionPane.showInputDialog("Ingrese Nombre"));
- estudiante[i].setEdad(Integer.valueOf(JOptionPane.showInputDialog("Ingrese Edad")));
- estudiante[i].setCurso(JOptionPane.showInputDialog("Ingrese Curso (escribir nombre de curso)"));
+ alumno[i] = new Estudiantes();
+ alumno[i].setLegajo(Integer.valueOf(JOptionPane.showInputDialog("Ingrese legajo")));
+ alumno[i].setNombre(JOptionPane.showInputDialog("Ingrese Nombre"));
+ alumno[i].setEdad(Integer.valueOf(JOptionPane.showInputDialog("Ingrese Edad")));
+ alumno[i].setCurso(JOptionPane.showInputDialog("Ingrese Curso (escribir nombre de curso)"));
     
     
 }
 for(i=0; i<a; i++){ 
-JOptionPane.showMessageDialog(null, "Legajo: "+estudiante[i].getLegajo()+"\nNombre: "+estudiante[i].getNombre()+"\nEdad: "+estudiante[i].getEdad()+"\nCurso: "+estudiante[i].getCurso());
+JOptionPane.showMessageDialog(null, "Legajo: "+alumno[i].getLegajo()+"\nNombre: "+alumno[i].getNombre()+"\nEdad: "+alumno[i].getEdad()+"\nCurso: "+alumno[i].getCurso());
 
 }
 
